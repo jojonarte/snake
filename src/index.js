13c7c730324bcd03d9c2c2bb6a0121f0c15f80ad
jojonarte <joselitonarte@gmail.com>
2020-08-10 22:52:31 +0800

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('canvas');
+
+const context = canvas.getContext('2d');
+context.fillStyle = 'orange'
+context.fillRect(0, 0, 150, 150);
+
+function generateField(context) {
+
+}
